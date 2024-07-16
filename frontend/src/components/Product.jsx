66842b909a189f0583da85cafab7201fdaa2d0a1
Sushan Shakya <shakyasushan0@gmail.com>
2024-07-16 12:19:38 +0545

    added product pages

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,20 @@
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+function Product({ product }) {
+  return (
+    <Card className="my-3 p-3">
+      <Card.Img src={product.image} variant="top" />
+      <Card.Body>
+        <Card.Text as="div">
+          <strong>{product.name}</strong>
+        </Card.Text>
+        <Card.Text as="div">
+          <Rating value={product.rating} text={product.numReviews} />
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+export default Product;
