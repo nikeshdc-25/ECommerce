c95f9a5c2e3f3c4df92b8d2dda937b9f112e68c2
name <example@gmail.com>
2024-08-16 22:30:00 +0545

    test2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import { Outlet } from "react-router-dom"
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
 
 
 
@@ -13,6 +14,7 @@ function App() {
     <Container className="my-3">
       <Outlet/>
     </Container>
+    <Footer />
     <ToastContainer />
     </>
   )
